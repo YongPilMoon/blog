8a1edcd24b0b8a40f8f37a92bb63d9d3d41009e0
yongpilmoon <m71661@gmail.com>
2023-02-08 13:40:42 +0900

    feat: codeblock 스타일 수정

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -1,14 +1,14 @@
-import { rainbow } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
-import SyntaxHighlighter from 'react-syntax-highlighter'
+import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism/'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 
 function CodeBlock({ children }) {
   return (
     <div className="relative">
       <SyntaxHighlighter
         showLineNumbers
-        style={rainbow}
-        codeTagProps={{ style: { background: 'none' } }}
+        style={a11yDark}
         language="javascript"
+        customStyle={{ fontSize: '1em' }}
       >
         {children}
       </SyntaxHighlighter>
