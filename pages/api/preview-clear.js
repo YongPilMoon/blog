1b81804bb9edb15d4c48b036068cb365078e8723
yongpilmoon <m71661@gmail.com>
2023-02-25 14:00:40 +0900

    feat: 사용되지 않는 파일 제거

--- pages/api/preview-clear.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.clearPreviewData({});
-
-  res.status(200).json({ message: "cookie clear" });
-}
--- pages/api/preview.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.query.token !== "nick" || !req.query.post) {
-    return res.status(401).json({ message: "invalid Token" });
-  }
-
-  res.setPreviewData({});
-
-  res.redirect(`/posts/${req.query.post}`);
-}
